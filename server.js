771d7d47f978d7a9e028e10710acc7a2cbbcbd75
Juan Barra <contacto@juanbarra.cl>
2021-10-14 23:58:27 -0300

    feat: add html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer(( request, response) =>{
     
-    response.setHeader('Content-Type', 'text/plain');
+    response.setHeader('Content-Type', 'text/html');
     response.write('<h1>Hola mundo</h1>');
     response.end();
 });
